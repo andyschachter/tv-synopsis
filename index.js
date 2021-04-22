3cc50fd9327e8642bb30790f6ba428ef865959f0
Andy Schachter <andy.schachter@gmail.com>
2021-04-21 20:26:15 -0400

    added get and all requests for index and added header for index page

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const app = express()
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
+app.get('/', (request, response) => {
+  response.render('index', { shows })
+})
+
+app.all('*', (request, response) => {
+  return response.sendStatus(404)
+})
+
 app.listen(1340, () => {
   console.log('Listening on 1340...') // eslint-disable-line no-console
 })
